fa6bedb319243f2e4c341b791ab523602f5a7f3b
david aragao <davidaragao@gmail.com>
2013-08-11 00:04:57 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pocketprofile::Application.config.secret_token = '89b2de5a4c9da5086394d16249d8564e6d2b5038a852dabd77ce4093271656bacd085da9285885a0a2a74fd6efe72fcfaad931bd01583011a0e0a8d041ffd6e6'
